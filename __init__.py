05b001110d39156808dc4390979d4b3a59db920c
Nicholas Reynolds <nichoreynolds@gmail.com>
2021-12-16 20:25:17 -0600

    Experiments with AddonPrefs

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 bl_info = {
-    "name": "Schmod Blender Binds",
+    "name": "Schmod Blender Bindings",
     "description": "Schmod Hotkey system for making Blender usage less ass",
     "author": "Nicholas Reynolds",
     "version": (1, 0),
@@ -8,12 +8,15 @@ bl_info = {
 }
 
 import bpy
+from bpy.props import StringProperty, IntProperty, BoolProperty, EnumProperty
 import mathutils
+import rna_keymap_ui
 import os
 import math
 from pathlib import Path
 
-class SchmodHotkeySwitcher(bpy.types.Panel):
+# Operators
+class SchmodBindingSwapper(bpy.types.Panel):
     """Hotkey Switcher"""
     bl_label = "Hotkey Switcher"
     bl_space_type = 'VIEW_3D'
@@ -48,6 +51,7 @@ class ContextualViewAxisSwap(bpy.types.Operator):
             if is_view_persp:
                 self.set_view_axis(self.swap_type)
             else:
+                bpy.ops.view3d.view_persportho()
                 cur_view_axis = self.get_ortho_view()
                 if cur_view_axis != '':
                     if self.swap_type == cur_view_axis:
@@ -88,8 +92,13 @@ class ContextualViewAxisSwap(bpy.types.Operator):
                             (r(math.pi/2), 0.0, r(-math.pi)) : 'BACK',
                             (r(-math.pi/2), r(math.pi/2), 0.0) : 'LEFT',
                             (r(-math.pi/2), r(-math.pi/2), 0.0) : 'RIGHT'}
-        
-        return orientation_dict[vec]
+        out = None
+        try:
+            out = orientation_dict[vec]
+        except:
+            out = 'TOP'
+
+        return out
 
     def get_opposing_view(self, view_axis):
         view_pair = {
@@ -107,28 +116,50 @@ class ContextualViewAxisSwap(bpy.types.Operator):
         bpy.ops.view3d.view_axis(type=view_axis)
         print (view_axis)
 
-operator_classes = [SchmodHotkeySwitcher, ContextualViewAxisSwap]
+# Hotkeys
 addon_keymaps = []
+
+# https://github.com/pitiwazou/Scripts-Blender/blob/Older-Scripts/addon_keymap_template
+def get_hotkey_entry_item(km, kmi_name, kmi_value):
+    '''
+    returns hotkey of specific type, with specific properties.name (keymap is not a dict, so referencing by keys is not enough
+    if there are multiple hotkeys!)
+    '''
+    for i, km_item in enumerate(km.keymap_items):
+        if km.keymap_items.keys()[i] == kmi_name:
+            if km.keymap_items[i].properties.name == kmi_value:
+                return km_item
+    return None
+
+def get_addon_preferences():
+    ''' quick wrapper for referencing addon preferences '''
+    addon_preferences = bpy.context.user_preferences.addons[__name__].preferences
+    return addon_preferences
 
 def add_hotkeys():
     wm = bpy.context.window_manager
     kc = wm.keyconfigs.addon
     if kc:
+        # Swap Perspective/Ortho
+        kmPerspOrtho = kc.keymaps.new(name='3D View', space_type='VIEW_3D')
+        kmiPerspOrtho = kmPerspOrtho.keymap_items.new('view3d.view_persportho', 'NUMPAD_1', 'PRESS', key_modifier='SPACE')
+        addon_keymaps.append((kmPerspOrtho, kmiPerspOrtho))
+
         # Contextual Top/Bottom
         kmTopBottom = kc.keymaps.new(name='3D View', space_type='VIEW_3D')
-        kmiTopBottom = kmTopBottom.keymap_items.new('wm.contextual_view_axis_swap', 'NUMPAD_1', 'PRESS', key_modifier='SPACE')
+        kmiTopBottom = kmTopBottom.keymap_items.new('wm.contextual_view_axis_swap', 'NUMPAD_2', 'PRESS', key_modifier='SPACE')
         kmiTopBottom.properties.swap_type = 'TOP'
-        addon_keymaps.append((kmTopBottom,kmiTopBottom))
+        addon_keymaps.append((kmTopBottom, kmiTopBottom))
 
         # Contextual Front/Back
         kmFrontBack = kc.keymaps.new(name='3D View', space_type='VIEW_3D')
-        kmiFrontBack = kmFrontBack.keymap_items.new('wm.contextual_view_axis_swap', 'NUMPAD_2', 'PRESS', key_modifier='SPACE')
+        kmiFrontBack = kmFrontBack.keymap_items.new('wm.contextual_view_axis_swap', 'NUMPAD_3', 'PRESS', key_modifier='SPACE')
         kmiFrontBack.properties.swap_type = 'FRONT'
         addon_keymaps.append((kmFrontBack, kmiFrontBack))
 
         # Contextual Left/Right
         kmLeftRight = kc.keymaps.new(name='3D View', space_type='VIEW_3D')
-        kmiLeftRight = kmLeftRight.keymap_items.new('wm.contextual_view_axis_swap', 'NUMPAD_3', 'PRESS', key_modifier='SPACE')
+        kmiLeftRight = kmLeftRight.keymap_items.new('wm.contextual_view_axis_swap', 'NUMPAD_4', 'PRESS', key_modifier='SPACE')
         kmiLeftRight.properties.swap_type = 'LEFT'
         addon_keymaps.append((kmLeftRight, kmiLeftRight))
 
@@ -158,6 +189,49 @@ def remove_hotkeys():
     
     addon_keymaps.clear()
 
+# Prefs
+class SchmodBlenderBindingsPrefs(bpy.types.AddonPreferences):
+    bl_idname = __name__
+
+    prefs_tabs: EnumProperty(items=(('general', "General", "General"), ('keymaps', "Keymaps", "Keymaps")), default='general')
+
+    """filepath: StringProperty(
+        name="Example File Path",
+        subtype='FILE_PATH',
+    )
+    number: IntProperty(
+        name="Example Number",
+        default=4,
+    )
+    boolean: BoolProperty(
+        name="Example Boolean",
+        default=False,
+    )"""
+
+    def draw(self, context):
+        layout = self.layout
+        wm = context.window_manager
+
+        row = layout.row(align=True)
+        row.prop(self, 'prefs_tabs', expand=True)
+
+        if self.prefs_tabs == 'general':
+            return
+
+        if self.prefs_tabs == 'keymaps':
+            box = layout.box()
+            col = box.column()
+
+            wm = context.window_manager
+            kc = wm.keyconfigs.addon
+
+            for km, kmi in addon_keymaps:
+                col.context_pointer_set("keymap", km)
+                rna_keymap_ui.draw_kmi([], kc, km, kmi, col, 0)
+
+# Registration
+operator_classes = [SchmodBindingSwapper, ContextualViewAxisSwap, SchmodBlenderBindingsPrefs]
+
 def register():
     for cls in operator_classes:
         bpy.utils.register_class(cls)
@@ -170,5 +244,6 @@ def unregister():
 
     remove_hotkeys()
 
+# Debug
 if __name__ == "__main__":
     register()
