c29716ea9d564d0d7bc972376dc49ef14f598653
Nicholas Reynolds <nichoreynolds@gmail.com>
2021-12-21 20:31:01 -0600

    Safety Commit

--- __init__.py
+++ __init__.py
@@ -16,7 +16,7 @@ import math
 from pathlib import Path
 
 # Operators
-class SchmodBindingSwapper(bpy.types.Panel):
+class SCHMOD_PT_SchmodBindingSwapper(bpy.types.Panel):
     """Hotkey Switcher"""
     bl_label = "Hotkey Switcher"
     bl_space_type = 'VIEW_3D'
@@ -36,14 +36,32 @@ class SchmodBindingSwapper(bpy.types.Panel):
         lyo_keymaps.operator('preferences.keyconfig_activate', text='', icon='BLENDER').filepath = str(blender_keymap)
         lyo_keymaps.operator('preferences.keyconfig_activate', text='', icon='EVENT_I').filepath = str(schmod_keymap)
 
-class ContextualViewAxisSwap(bpy.types.Operator):
+class SCHMOD_OT_SwapPerspOrtho(bpy.types.Operator):
+    bl_idname = 'wm.swap_persp_ortho'
+    bl_label = 'Swap Perspective Ortho'
+
+    lookup: bpy.props.StringProperty(name='Lookup')
+
+    def execute(self, context):
+        bpy.ops.view3d.view_persportho()
+        return {'FINISHED'}
+
+class SCHMOD_OT_FocusOnSelection(bpy.types.Operator):
+    bl_idname = 'wm.focus_on_selection'
+    bl_label = 'Focus On Selection'
+
+    lookup: bpy.props.StringProperty(name='Lookup')
+
+    def execute(self, context):
+        bpy.ops.view3d.view_selected()
+        return {'FINISHED'}
+
+class SCHMOD_OT_ContextualViewAxisSwap(bpy.types.Operator):
     bl_idname = 'wm.contextual_view_axis_swap'
     bl_label = 'Contextual View Axis Swap'
 
+    lookup: bpy.props.StringProperty(name='Lookup')
     swap_type: bpy.props.StringProperty(name='Swap Type')
-
-    def __init__(self):
-        super(ContextualViewAxisSwap, self).__init__()
 
     def execute(self, context):
         if self.swap_type is not None:
@@ -127,59 +145,56 @@ def get_hotkey_entry_item(km, kmi_name, kmi_value):
     '''
     for i, km_item in enumerate(km.keymap_items):
         if km.keymap_items.keys()[i] == kmi_name:
+            print('Keymap Key: {0}'.format(km.keymap_items.keys()[i]))
             if km.keymap_items[i].properties.name == kmi_value:
+                print('Keymap Name: {0}'.format(km.keymap_items[i].properties.name))
                 return km_item
     return None
 
-def get_addon_preferences():
-    ''' quick wrapper for referencing addon preferences '''
-    addon_preferences = bpy.context.user_preferences.addons[__name__].preferences
-    return addon_preferences
-
 def add_hotkeys():
-    wm = bpy.context.window_manager
-    kc = wm.keyconfigs.addon
-    if kc:
+    win_manager = bpy.context.window_manager
+    key_configs = win_manager.keyconfigs.addon
+    if key_configs:
         # Swap Perspective/Ortho
-        kmPerspOrtho = kc.keymaps.new(name='3D View', space_type='VIEW_3D')
-        kmiPerspOrtho = kmPerspOrtho.keymap_items.new('view3d.view_persportho', 'NUMPAD_1', 'PRESS', key_modifier='SPACE')
+        kmPerspOrtho = key_configs.keymaps.new(name='3D View', space_type='VIEW_3D')
+        kmiPerspOrtho = kmPerspOrtho.keymap_items.new('wm.swap_persp_ortho', 'NUMPAD_1', 'PRESS', key_modifier='SPACE')
         addon_keymaps.append((kmPerspOrtho, kmiPerspOrtho))
 
         # Contextual Top/Bottom
-        kmTopBottom = kc.keymaps.new(name='3D View', space_type='VIEW_3D')
+        kmTopBottom = key_configs.keymaps.new(name='3D View', space_type='VIEW_3D')
         kmiTopBottom = kmTopBottom.keymap_items.new('wm.contextual_view_axis_swap', 'NUMPAD_2', 'PRESS', key_modifier='SPACE')
         kmiTopBottom.properties.swap_type = 'TOP'
         addon_keymaps.append((kmTopBottom, kmiTopBottom))
 
         # Contextual Front/Back
-        kmFrontBack = kc.keymaps.new(name='3D View', space_type='VIEW_3D')
+        kmFrontBack = key_configs.keymaps.new(name='3D View', space_type='VIEW_3D')
         kmiFrontBack = kmFrontBack.keymap_items.new('wm.contextual_view_axis_swap', 'NUMPAD_3', 'PRESS', key_modifier='SPACE')
         kmiFrontBack.properties.swap_type = 'FRONT'
         addon_keymaps.append((kmFrontBack, kmiFrontBack))
 
         # Contextual Left/Right
-        kmLeftRight = kc.keymaps.new(name='3D View', space_type='VIEW_3D')
+        kmLeftRight = key_configs.keymaps.new(name='3D View', space_type='VIEW_3D')
         kmiLeftRight = kmLeftRight.keymap_items.new('wm.contextual_view_axis_swap', 'NUMPAD_4', 'PRESS', key_modifier='SPACE')
         kmiLeftRight.properties.swap_type = 'LEFT'
         addon_keymaps.append((kmLeftRight, kmiLeftRight))
 
         # Focus
-        kmFocus = kc.keymaps.new(name='3D View', space_type='VIEW_3D')
+        kmFocus = key_configs.keymaps.new(name='3D View', space_type='VIEW_3D')
         kmiFocus = kmFocus.keymap_items.new('view3d.view_selected', 'F', 'PRESS')
         addon_keymaps.append((kmFocus, kmiFocus))
         
         # Rotate
-        kmRotateView = kc.keymaps.new(name='3D View', space_type='VIEW_3D')
+        kmRotateView = key_configs.keymaps.new(name='3D View', space_type='VIEW_3D')
         kmiRotateView = kmRotateView.keymap_items.new('view3d.rotate', 'LEFTMOUSE', 'PRESS', key_modifier='SPACE')
         addon_keymaps.append((kmRotateView, kmiRotateView))
 
         # Pan
-        kmPanView = kc.keymaps.new(name='3D View', space_type='VIEW_3D')
+        kmPanView = key_configs.keymaps.new(name='3D View', space_type='VIEW_3D')
         kmiPanView = kmPanView.keymap_items.new('view3d.move', 'MIDDLEMOUSE', 'PRESS', key_modifier='SPACE')
         addon_keymaps.append((kmPanView, kmiPanView))
 
         # Zoom
-        kmZoomView = kc.keymaps.new(name='3D View', space_type='VIEW_3D')
+        kmZoomView = key_configs.keymaps.new(name='3D View', space_type='VIEW_3D')
         kmiZoomView = kmZoomView.keymap_items.new('view3d.zoom', 'RIGHTMOUSE', 'PRESS', key_modifier='SPACE')
         addon_keymaps.append((kmZoomView, kmiZoomView))
 
@@ -195,20 +210,8 @@ class SchmodBlenderBindingsPrefs(bpy.types.AddonPreferences):
 
     prefs_tabs: EnumProperty(items=(('general', "General", "General"), ('keymaps', "Keymaps", "Keymaps")), default='general')
 
-    """filepath: StringProperty(
-        name="Example File Path",
-        subtype='FILE_PATH',
-    )
-    number: IntProperty(
-        name="Example Number",
-        default=4,
-    )
-    boolean: BoolProperty(
-        name="Example Boolean",
-        default=False,
-    )"""
-
     def draw(self, context):
+        return
         layout = self.layout
         wm = context.window_manager
 
@@ -223,14 +226,27 @@ class SchmodBlenderBindingsPrefs(bpy.types.AddonPreferences):
             col = box.column()
 
             wm = context.window_manager
-            kc = wm.keyconfigs.addon
+            kc = wm.keyconfigs.user
+            km = kc.keymaps['3D View']
 
-            for km, kmi in addon_keymaps:
+            kmiSwapOrtho = get_hotkey_entry_item(km, 'wm.swap_persp_ortho', SwapPerspOrtho.bl_label)
+            print(kmiSwapOrtho)
+
+            if kmiSwapOrtho:
                 col.context_pointer_set("keymap", km)
-                rna_keymap_ui.draw_kmi([], kc, km, kmi, col, 0)
+                rna_keymap_ui.draw_kmi([], kc, km, kmiSwapOrtho, col, 0)
+            else:
+                #print ('Nope')
+                #col.label("No hotkey entry found")
+                #col.operator(Template_Add_Hotkey.bl_idname, text = "Add hotkey entry", icon = 'ZOOMIN')
+                pass
 
 # Registration
-operator_classes = [SchmodBindingSwapper, ContextualViewAxisSwap, SchmodBlenderBindingsPrefs]
+operator_classes = [
+        SCHMOD_PT_SchmodBindingSwapper, 
+        SCHMOD_OT_SwapPerspOrtho,
+        SCHMOD_OT_ContextualViewAxisSwap
+        ]
 
 def register():
     for cls in operator_classes:
